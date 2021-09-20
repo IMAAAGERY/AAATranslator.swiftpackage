eb86ecbd4670120c8228c85caede837948de1d4c
Léo <4069471+leolelego@users.noreply.github.com>
2021-09-20 15:39:04 +0200

    Update

--- Sources/AAATranslator/AAATranslator.swift
+++ Sources/AAATranslator/AAATranslator.swift
@@ -1,4 +1,5 @@
 public struct AAATranslator {
+    public static let version = "1.0"
     public var text = "Hello, World!"
     
 }
--- Tests/AAATranslatorTests/AAATranslatorTests.swift
+++ Tests/AAATranslatorTests/AAATranslatorTests.swift
@@ -3,6 +3,7 @@
 
     final class AAATranslatorTests: XCTestCase {
         func testExample() {
+            print("Text is \(AAATranslator().text)") 
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
