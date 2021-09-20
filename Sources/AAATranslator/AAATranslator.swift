20303f61a0bad280eca3df5f887c959195ab3142
Léo <4069471+leolelego@users.noreply.github.com>
2021-09-20 17:56:10 +0200

    Version 1.0

--- Sources/AAATranslator/AAATranslator.swift
+++ Sources/AAATranslator/AAATranslator.swift
@@ -1,5 +1,23 @@
-public struct AAATranslator {
-    public static let version = "1.0"
-    public var text = "Hello, World!"
+import Combine
+public class AAATranslator : ObservableObject {
+
+    let parser : AAATranslatorParser
+    @Published private(set) public  var data : [String:String] = [:] // Useless but usefull - don't know why
+    public init(_ parser:AAATranslatorParser){
+        self.parser = parser
+    }
+    
+    public func get(_ key:String) -> String {
+        print("Called")
+        return data[key] ?? "__NO VALUE FOR \(key)__"
+    }
+    
+    public func load(_ language:String) throws{
+        data = try parser.load(language)
+    }
+    
+    public func languagesSupported() -> [String] {
+        return parser.keys()
+    }
     
 }
---
+++ Sources/AAATranslator/AAATranslatorParserProtocol.swift
@@ -0,0 +1,48 @@
+//
+//  File.swift
+//  
+//
+//  Created by Léo on 20/09/2021.
+//
+
+import Foundation
+enum AAATranslatorParserError: Error {
+    case invalidFile
+}
+public protocol AAATranslatorParser {
+    
+    
+    init(files:[String:URL])
+    func load(_ key:String) throws -> [String:String]
+    func keys() -> [String]
+}
+
+public struct AAATranslatorParserDefault : AAATranslatorParser {
+    
+    internal let files : [String:URL]
+    public init(files: [String:URL]) {
+        self.files = files
+    }
+    
+    public func load(_ key: String) throws -> [String : String] {
+        
+        guard let url = files[key] else {
+            throw AAATranslatorParserError.invalidFile
+        }
+        
+        guard let jsonData = try? Data(contentsOf: url),
+              let items = try? JSONDecoder().decode([String:String].self, from: jsonData)
+        else {
+            throw AAATranslatorParserError.invalidFile
+        }
+        return items
+        
+    }
+    
+    public func keys() -> [String] {
+        return Array(files.keys)
+    }
+
+    
+    
+}
